2c476226b33090a2aa768419430cef9bb2ec6a11
vincentmegia <vincent.megia@gmail.com>
2020-08-07 20:50:47 +0800

    f01: method test1

--- src/main/java/com/vincentmegia/orderstore/repositories/OrderRepository.java
+++ src/main/java/com/vincentmegia/orderstore/repositories/OrderRepository.java
@@ -4,6 +4,9 @@ import com.vincentmegia.orderstore.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> test1();
 }
